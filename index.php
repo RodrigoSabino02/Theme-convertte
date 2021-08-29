d9e0741d11d69167ae3f9369b369e8caedb676cd
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-08-29 16:03:35 -0300

    delete page.php

--- index.php
+++ index.php
@@ -502,11 +502,3 @@
     </div>
   </div>
   <?php get_footer(); ?>
-
-  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5fa45342033e749f917aad36" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="js/mem-c1ad69.js" type="text/javascript"></script>
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
-
-
-</body>
-</html>
