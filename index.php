96f0474d94a893808adbe2ee772be16f2c2a2094
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-08-29 10:35:50 -0300

    footer.php

--- index.php
+++ index.php
@@ -546,61 +546,7 @@
       </div>
     </div>
   </div>
-  <div class="footer-3">
-    <div class="main-container">
-      <div class="footer-1-top">
-        <div class="main-container">
-          <div class="footer-1-top-row">
-            <a href="#" class="footer-1-logo w-inline-block"><img src="images/logo-mem2x.png" sizes="250px" srcset="images/logo-mem2x-p-500.png 500w, images/logo-mem2x.png 523w" alt="" class="image-7 logo"></a>
-            <ul role="list" class="social-icons-list w-list-unstyled">
-              <li class="list-item">
-                <a href="https://www.linkedin.com/company/santos-silva-gest%C3%A3o-empresarial/" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace3f980c72ea_linkedin.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://www.instagram.com/santossilvagestaoempresarial/" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace17750c7313_instagram.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://www.facebook.com/SantosSilvaGestaoEmpresarial" target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="https://uploads-ssl.webflow.com/5f731fdea86d2045a59944e9/5f731fde240ace3df40c72ce_facebook.svg" alt="" class="social-icon-2 social-icon-dark"></a>
-              </li>
-              <li class="list-item">
-                <a href="https://api.whatsapp.com/send?phone=+5521976499451&amp;text=Ol%C3%A1%21%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es." target="_blank" class="button-circle button-small bg-gray-3 w-inline-block"><img src="images/whatsapp.svg" alt="" class="social-icon-2 social-icon-dark iconwhatsapp"></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="footer-3-top-row">
-        <div class="footer-3-logo-and-menus">
-          <div class="w-layout-grid menu-grid-vertical">
-            <a href="https://spark-template.webflow.io/#Landing-Pages" class="hover-link text-white text-large">Início</a>
-            <a href="https://spark-template.webflow.io/#Company-Pages" class="hover-link text-white text-large">Notícias</a>
-            <a href="https://spark-template.webflow.io/#Pricing-Pages" class="hover-link text-white text-large">Matérias</a>
-            <a href="https://spark-template.webflow.io/#Account-Pages" class="hover-link text-white text-large">Podcasts</a>
-            <a href="https://spark-template.webflow.io/#Account-Pages" class="hover-link text-white text-large mimos">Mimos? Temos!</a>
-          </div>
-        </div>
-        <div class="container">
-          <h5 class="footer-form-heading">Contato</h5>
-          <p class="paragraph-2">(00) 0000-0000<br>falecom@mem.com.br<br><br><strong>Matriz<br></strong>Avenida nome da rua, 000 - Bairro, Cidade - UF, 00000-000</p>
-          <div class="div-block-4">
-            <a href="https://cursos.uniterset.org/ver/curso/formacao-de-auditores-de-responsabilidade-social/" target="_blank" class="button-6 small bt-borda bt-footer w-inline-block">
-              <div class="text-block-21 whtas-txt-2 txt-menu-button text-block-22 bt-footer">Querio anunciar neste site!</div>
-            </a>
-            <a href="https://cursos.uniterset.org/ver/curso/formacao-de-auditores-de-responsabilidade-social/" target="_blank" class="button-6 small bt-borda w-inline-block">
-              <div class="text-block-21 whtas-txt-2 txt-menu-button text-block-22">Contato</div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="footer-bottom-row footer-3-bottom-row">
-        <div class="footer-bottom-links">
-          <a href="#" class="row-link text-white">Políticas legais</a>
-          <div>© Criado por <a href="https://www.webflow.com/" target="_blank" class="hover-link text-white">Visualpage</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php get_footer(); ?>
 
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5fa45342033e749f917aad36" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/mem-c1ad69.js" type="text/javascript"></script>
