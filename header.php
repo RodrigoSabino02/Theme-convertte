27a757992f5cf081c7b869feac07a92f194fb4a0
Rodrigo Sabino <rodrigosabinosdm@gmail.com>
2021-08-29 10:39:07 -0300

    Fixing header.php

--- header.php
+++ header.php
@@ -15,3 +15,48 @@
   <link href="images/webclip.png" rel="apple-touch-icon">
 </head>
 <body>
+
+<div data-collapse="medium" data-animation="default" data-duration="400" id="Navigation" role="banner" class="nav-bar-v2 w-nav">
+    <a href="#" class="nav-brand-v2 w-nav-brand"><img src="images/escolas-mem.png" sizes="(max-width: 479px) 78.96875px, 105.296875px" srcset="images/escolas-mem-p-500.png 500w, images/escolas-mem-p-800.png 800w, images/escolas-mem-p-1080.png 1080w, images/escolas-mem.png 1111w" alt="" class="nav-logo"></a>
+    <div class="nav-space"></div>
+    <nav role="navigation" class="nav-menu-v2 w-nav-menu">
+      <form action="/search" class="search-form w-form"><input type="search" class="search-form-input w-input" maxlength="256" name="query" placeholder=" Search..." id="search" required=""><input type="submit" value=" " class="search-button w-button"></form>
+      <a href="#" class="nav-link w-nav-link">Home</a>
+      <a href="#" class="nav-link w-nav-link">Notícias</a>
+      <a href="#" class="nav-link w-nav-link">Matérias</a>
+      <a href="#" class="nav-link w-nav-link">Filmes</a>
+      <a href="#" class="nav-link w-nav-link">Podcasts</a>
+    </nav>
+    <div class="nav-right-v2">
+      <a href="#" class="button subscribe-button w-button">Receba Mimos</a>
+    </div>
+    <div class="menu-button-v2 w-nav-button">
+      <div class="menu-icon">
+        <div class="menu-line-top"></div>
+        <div class="menu-line-middle"></div>
+        <div class="menu-line-bottom"></div>
+      </div>
+    </div>
+    <div data-w-id="b6a32dce-ecac-cd1e-d4b6-2cef1b7a197d" class="menu-button-v2 side-menu-button">
+      <div class="menu-icon">
+        <div class="menu-line-top-2"></div>
+        <div class="menu-line-middle-2"></div>
+        <div class="menu-line-bottom-2"></div>
+      </div>
+    </div>
+    <div class="side-menu">
+      <div class="full-height-menu-contents">
+        <a href="#" class="w-inline-block"><img src="images/logo-mem2x.png" width="200" sizes="200px" srcset="images/logo-mem2x-p-500.png 500w, images/logo-mem2x.png 523w" alt=""></a>
+        <div class="full-height-menu-links">
+          <a href="#" class="full-height-menu-link">Home</a>
+          <a href="https://spark-template.webflow.io/#Landing-Pages" class="full-height-menu-link">Notícias</a>
+          <a href="#" class="full-height-menu-link">Matérias</a>
+          <a href="#" class="full-height-menu-link">Podcasts</a>
+          <a href="#" class="full-height-menu-link">Mimos</a>
+          <a href="#" class="full-height-menu-link">Contato</a>
+        </div>
+        <div class="full-height-menu-footer"></div>
+      </div>
+    </div>
+    <div class="side-menu-overlay"></div>
+  </div>
